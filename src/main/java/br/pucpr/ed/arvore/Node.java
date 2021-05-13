437f7d46f6abc4c3dddd40fbf718fb672c816ff8
LeonardoPeron <leonardo.peron@lb2.com.br>
2021-05-12 23:28:36 -0300

    first commit

---
+++ src/main/java/br/pucpr/ed/arvore/Node.java
@@ -0,0 +1,42 @@
+package br.pucpr.ed.arvore;
+
+public class Node<T> {
+    protected T dados;
+    protected Node filhoEsquerdo;
+    protected Node filhoDireito;
+
+    public Node(T dados) {
+        this.dados = dados;
+    }
+
+    public T getDados() {
+        return dados;
+    }
+
+    public void setDados(T dados) {
+        this.dados = dados;
+    }
+
+    public Node getFilhoEsquerdo() {
+        return filhoEsquerdo;
+    }
+
+    public void setFilhoEsquerdo(Node filhoEsquerdo) {
+        this.filhoEsquerdo = filhoEsquerdo;
+    }
+
+    public Node getFilhoDireito() {
+        return filhoDireito;
+    }
+
+    public void setFilhoDireito(Node filhoDireito) {
+        this.filhoDireito = filhoDireito;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+            "dados=" + dados +
+            '}';
+    }
+}
